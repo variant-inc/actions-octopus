9987b4e8c6df1ad661c99e422aaa301c9585f21c
Vibin Daniel <vibind@drivevariant.com>
2021-02-03 15:59:47 -0500

    [LAZY-43] bare url
    [LAZY-43] a

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -32,52 +32,51 @@ elseif (${env:GITVERSION_BRANCHNAME} -match "${env:FEATURE_CHANNEL_BRANCHES}")
 } else {
   exit 0;
 }
-$channelName
 
-# $deployScriptsPath = [System.IO.Path]::GetFullPath((Join-Path ${env:GITHUB_WORKSPACE} ${env:DEPLOY_SCRIPTS_PATH}))
+$deployScriptsPath = [System.IO.Path]::GetFullPath((Join-Path ${env:GITHUB_WORKSPACE} ${env:DEPLOY_SCRIPTS_PATH}))
 
-# mkdir -p ./packages/
-# ce octo pack --id="${env:PROJECT_NAME}" `
-#   --format="Zip" --version="${env:VERSION}" `
-#   --basePath="$deployScriptsPath" --outFolder="./packages"
+mkdir -p ./packages/
+ce octo pack --id="${env:PROJECT_NAME}" `
+  --format="Zip" --version="${env:VERSION}" `
+  --basePath="$deployScriptsPath" --outFolder="./packages"
 
-# ce octo push --package="./packages/${env:PROJECT_NAME}.${env:VERSION}.zip" `
-#   --space="${env:SPACE_NAME}" `
-#   --overwrite-mode=OverwriteExisting
+ce octo push --package="./packages/${env:PROJECT_NAME}.${env:VERSION}.zip" `
+  --space="${env:SPACE_NAME}" `
+  --overwrite-mode=OverwriteExisting
 
-# $commitMessage = git log -1 --pretty=oneline
-# $commitMessage = $commitMessage -replace "${env:GITHUB_SHA} ", ""
-# Write-Information "Commit Message: $commitMessage"
-# $jsonBody = @{
-#   BuildEnvironment = "GitHub Actions"
-#   Branch           = "${env:GITVERSION_BRANCHNAME}"
-#   BuildNumber      = "${env:GITHUB_RUN_NUMBER}"
-#   BuildUrl         = "https://github.com/${env:GITHUB_REPOSITORY}/actions/runs/${env:GITHUB_RUN_ID}"
-#   VcsCommitNumber  = "${env:GITHUB_SHA}"
-#   VcsType          = "Git"
-#   VcsRoot          = "https://github.com/${env:GITHUB_REPOSITORY}.git"
-#   Commits          = @(
-#     @{
-#       Id      = "${env:GITHUB_SHA}"
-#       LinkUrl = "https://github.com/${env:GITHUB_REPOSITORY}/commit/${env:GITHUB_SHA}"
-#       Comment = "$commitMessage"
-#     }
-#   )
-# } | ConvertTo-Json -Depth 10 -Compress
+$commitMessage = git log -1 --pretty=oneline
+$commitMessage = $commitMessage -replace "${env:GITHUB_SHA} ", ""
+Write-Information "Commit Message: $commitMessage"
+$jsonBody = @{
+  BuildEnvironment = "GitHub Actions"
+  Branch           = "${env:GITVERSION_BRANCHNAME}"
+  BuildNumber      = "${env:GITHUB_RUN_NUMBER}"
+  BuildUrl         = "https://github.com/${env:GITHUB_REPOSITORY}/actions/runs/${env:GITHUB_RUN_ID}"
+  VcsCommitNumber  = "${env:GITHUB_SHA}"
+  VcsType          = "Git"
+  VcsRoot          = "https://github.com/${env:GITHUB_REPOSITORY}.git"
+  Commits          = @(
+    @{
+      Id      = "${env:GITHUB_SHA}"
+      LinkUrl = "https://github.com/${env:GITHUB_REPOSITORY}/commit/${env:GITHUB_SHA}"
+      Comment = "$commitMessage"
+    }
+  )
+} | ConvertTo-Json -Depth 10 -Compress
 
-# New-Item "buildinformation.json" -ItemType File
-# Set-Content -Path "buildinformation.json" -Value $jsonBody
+New-Item "buildinformation.json" -ItemType File
+Set-Content -Path "buildinformation.json" -Value $jsonBody
 
-# ce octo build-information `
-#   --package-id="${env:PROJECT_NAME}" `
-#   --file="buildinformation.json" `
-#   --version="${env:VERSION}" `
-#   --space="${env:SPACE_NAME}" `
-#   --overwrite-mode=OverwriteExisting
+ce octo build-information `
+  --package-id="${env:PROJECT_NAME}" `
+  --file="buildinformation.json" `
+  --version="${env:VERSION}" `
+  --space="${env:SPACE_NAME}" `
+  --overwrite-mode=OverwriteExisting
 
-# ce octo create-release `
-#   --project="${env:PROJECT_NAME}" `
-#   --packageVersion="${env:VERSION}" `
-#   --releaseNumber="${env:VERSION}" `
-#   --space="${env:SPACE_NAME}" `
-#   --channel="$channelName"
+ce octo create-release `
+  --project="${env:PROJECT_NAME}" `
+  --packageVersion="${env:VERSION}" `
+  --releaseNumber="${env:VERSION}" `
+  --space="${env:SPACE_NAME}" `
+  --channel="$channelName"
