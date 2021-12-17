78cd8b01818753cd645ef8ff7f463fd9e82759a1
Hitesh Patel <hpatel@hiteshp.me>
2021-12-17 11:09:41 -0500

    use cake-runner

--- start.ps1
+++ start.ps1
@@ -1,6 +1,11 @@
 [CmdLetBinding()]
 [Diagnostics.CodeAnalysis.SuppressMessage("PSAvoidGlobalVars", '')]
-Param()
+Param(
+  $NugetUser = $env:GITHUB_ACTOR,
+  $NugetToken = $env:GITHUB_PACKAGES_TOKEN,
+  $RepositoryRoot = $env:GITHUB_WORKSPACE,
+  $TaskRunnerVersion = $env:INPUT_TASK_RUNNER_VERSION
+)
 
 $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
@@ -27,6 +32,20 @@ function CommandAliasFunction
 }
 
 Set-Alias -Name ce -Value CommandAliasFunction -Scope script
+
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments', '', Scope = 'Function')]
+$variantApiDeployYamlPath = [System.IO.Path]::GetFullPath((Join-Path ${RepositoryRoot} ".variant/deploy/"))
+$deployYamlsFound = Get-ChildItem -Path $variantApiDeployYamlPath -Filter "*.yaml" -Recurse
+if ($deployYamlsFound.Count -gt 0)
+{
+  Set-Location ${RepositoryRoot}
+  dotnet nuget add source --username "${NugetUser}" --password "${NugetToken}" --store-password-in-clear-text --name github "https://nuget.pkg.github.com/variant-inc/index.json"
+  ce dotnet nuget update source github -u "${NugetUser}" -p "${NugetToken}" --store-password-in-clear-text -s "https://nuget.pkg.github.com/variant-inc/index.json"
+  ce dotnet new tool-manifest --force
+  ce dotnet tool install --version "${TaskRunnerVersion}" --no-cache Variant.Cake.Runner
+  ce dotnet variant-cake-runner --verbosity diagnostic --target ReleaseCreator --verbosity diagnostic
+  Exit
+}
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments', '', Scope = 'Function')]
 $octoYamlPath = [System.IO.Path]::GetFullPath((Join-Path ${env:GITHUB_WORKSPACE} ".octopus/workflow/octopus.yaml"))
