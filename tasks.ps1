a37ddcae5f688521da0e09702f5458aba84e6dbd
Naveen <naveenp@drivevariant.com>
2022-04-01 13:42:55 -0400

    test path

--- tasks.ps1
+++ tasks.ps1
@@ -11,21 +11,18 @@ $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
 $WarningPreference = "SilentlyContinue"
 
-Trap
-{
+Trap {
   Write-Error $_.InvocationInfo.ScriptName -ErrorAction Continue
   $line = "$($_.InvocationInfo.ScriptLineNumber): $($_.InvocationInfo.Line)"
   Write-Error $line -ErrorAction Continue
   Write-Error $_
 }
-function CommandAliasFunction
-{
+function CommandAliasFunction {
   Write-Information ""
   Write-Information "$args"
   $cmd, $args = $args
   & "$cmd" $args
-  if ($LASTEXITCODE)
-  {
+  if ($LASTEXITCODE) {
     throw "Exception Occured"
   }
   Write-Information ""
@@ -35,9 +32,16 @@ Set-Alias -Name ce -Value CommandAliasFunction -Scope script
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments', '', Scope = 'Function')]
 $variantApiDeployYamlPath = [System.IO.Path]::GetFullPath((Join-Path ${RepositoryRoot} ".variant/deploy/"))
-$deployYamlsFound = Get-ChildItem -Path $variantApiDeployYamlPath -Filter "*.yaml" -Recurse
-if ($deployYamlsFound.Count -gt 0)
-{
+$testPath = Test-Path -Path $variantApiDeployYamlPath
+Write-Information "Deploy folder exists in .variant directory: $testPath"
+if ($testPath -eq $true) {
+  $deployYamlsFound = Get-ChildItem -Path $variantApiDeployYamlPath -Filter "*.yaml" -Recurse
+}
+else {
+  throw "Deploy folder does not exists"
+}
+
+if ($deployYamlsFound.Count -gt 0) {
   Set-Location ${RepositoryRoot}
   dotnet nuget add source --username "${NugetUser}" --password "${NugetToken}" --store-password-in-clear-text --name github "https://nuget.pkg.github.com/variant-inc/index.json"
   ce dotnet nuget update source github -u "${NugetUser}" -p "${NugetToken}" --store-password-in-clear-text -s "https://nuget.pkg.github.com/variant-inc/index.json"
