3c99c49b262df1723cc34d749d431478c563e2e9
Luka Baruškin <63529189+zumic96@users.noreply.github.com>
2022-10-31 14:15:56 +0100

    F/cloud 1783 sonar (#71)
    * add sonar quality gate check for default branch

--- tasks.ps1
+++ tasks.ps1
@@ -30,6 +30,29 @@ function CommandAliasFunction {
 
 Set-Alias -Name ce -Value CommandAliasFunction -Scope script
 
+Write-Output "Starting Sonar check"
+if ($env:INPUT_DEFAULT_BRANCH -eq $env:GITVERSION_BRANCHNAME){
+  $sonarCheckUrl = "https://sonarcloud.io/api/qualitygates/project_status?projectKey=$env:SONAR_PROJECT_KEY&branch=$env:GITVERSION_BRANCHNAME"
+  $headers = @{
+    'Authorization' = 'Bearer ' + $env:SONAR_TOKEN
+    'Accept'        = 'application/json'
+  }
+  try {
+    $Response = Invoke-RestMethod -Uri $sonarCheckUrl -Headers $headers -Method GET
+    $Response | ConvertTo-Json
+    if ($Response.projectStatus.status -eq "OK") {
+      Write-Output "Sonnar scan quality gate passed. Continuing with the deployment."
+    } else {
+      Write-Output "Sonnar scan quality gate failed. Stopping the deployment."
+      exit 1
+    }
+  }
+  catch {
+    Write-Output "Skipping sonar check as project: $env:SONAR_PROJECT_KEY doesn't exist."
+  }
+}
+Write-Output "Sonar check done"
+
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments', '', Scope = 'Function')]
 $variantApiDeployYamlPath = [System.IO.Path]::GetFullPath((Join-Path ${RepositoryRoot} ".variant/deploy/"))
 
