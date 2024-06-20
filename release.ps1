065560fe4edaf195fcf20c3f91124e9da1b7ea2f
Parul Pachori <ppachori@usxpress.com>
2024-06-21 01:09:38 +0530

    [CLOUD-2504] Added Build Information to v1

--- release.ps1
+++ release.ps1
@@ -1,15 +1,6 @@
-if (Test-Path -Path $octoYamlPath -PathType Leaf)
-{
-  Write-Output "Gathering Space/Project from octopus.yaml"
-  $SPACE_NAME = ce yq eval .SpaceName $octoYamlPath
-  $PROJECT_NAME = ce yq eval .ProjectName $octoYamlPath
-}
-else
-{
-  Write-Output "Gathering Space/Project from workflow input"
-  $SPACE_NAME = $env:SPACE_NAME
-  $PROJECT_NAME = $env:PROJECT_NAME
-}
+Write-Output "Gathering Space/Project from workflow input"
+$SPACE_NAME = $env:SPACE_NAME
+$PROJECT_NAME = $env:PROJECT_NAME
 
 if (($null -eq $SPACE_NAME) -or ("" -eq $SPACE_NAME))
 {
@@ -53,26 +44,6 @@ ce octopus package upload --package="./packages/${PROJECT_NAME}.${env:VERSION}.z
 $commitMessage = git log -1 --pretty=oneline
 $commitMessage = $commitMessage -replace "${env:GITHUB_SHA} ", ""
 Write-Information "Commit Message: $commitMessage"
-Write-Output "Writing Build Information"
-$jsonBody = @{
-  BuildEnvironment = "actions-octopus:v1"
-  Branch           = "${env:GitVersion_BranchName}"
-  BuildNumber      = "${env:GITHUB_RUN_NUMBER}"
-  BuildUrl         = "https://github.com/${env:GITHUB_REPOSITORY}/actions/runs/${env:GITHUB_RUN_ID}"
-  VcsCommitNumber  = "${env:GITHUB_SHA}"
-  VcsType          = "Git"
-  VcsRoot          = "https://github.com/${env:GITHUB_REPOSITORY}.git"
-  Commits          = @(
-    @{
-      Id      = "${env:GITHUB_SHA}"
-      LinkUrl = "https://github.com/${env:GITHUB_REPOSITORY}/commit/${env:GITHUB_SHA}"
-      Comment = "$commitMessage"
-    }
-  )
-} | ConvertTo-Json -Depth 10 -Compress
-
-New-Item "buildinformation.json" -ItemType File -Force
-Set-Content -Path "buildinformation.json" -Value $jsonBody
 
 Write-Output "Creating Octopus Release"
 ce octopus release create `
