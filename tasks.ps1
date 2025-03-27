fda61916cb70e726ad90f0b54bcc0ea5ac4f3f76
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2025-03-27 13:56:30 -0400

    use rest api to download

--- tasks.ps1
+++ tasks.ps1
@@ -45,36 +45,21 @@ if ($deployYamlsFound.Count -gt 0) {
     $message = $(& dotnet tool install --version "${env:MAGE_RUNNER_VERSION}" --no-cache mage-runner ) 2>&1
     $env:MAGE_RUNNER_VERSION = [regex]::match($message, '\d+\.\d+\.\d+').Groups[0].Value
   }
-  else{
-    $env:MAGE_RELEASE = "pre-release"
-  }
   Write-Host "mage-runner version: $env:MAGE_RUNNER_VERSION"
 
-  $S3Bucket = $env:MAGE_S3_BUCKET
-  $MageRelease = $env:MAGE_RELEASE
-  $S3Key = "$MageRelease/mage-runner/mage-runner.$env:MAGE_RUNNER_VERSION.zip"
-  $ZipFilePath = "./mage/mage.zip"
+  $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+  $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "username", $env:AZ_DEVOPS_PAT)))
+  $headers.Add("Authorization", "Basic $base64AuthInfo")
 
-  Write-Host "Fetching $S3Key from s3://$S3Bucket/"
-  aws s3 cp "s3://$S3Bucket/$S3Key" $ZipFilePath --force
+  Invoke-RestMethod "https://pkgs.dev.azure.com/USXpress-Inc/CloudOps/_apis/packaging/feeds/Octopus/nuget/packages/mage-runner/versions/$($env:MAGE_RUNNER_VERSION)/content" -Method 'GET' -Headers $headers -OutFile mage.zip
 
-  if ($LASTEXITCODE -ne 0) {
-    Write-Error "Failed to fetch $S3Key from S3. AWS CLI returned exit code $LASTEXITCODE."
-    exit $LASTEXITCODE
-  }
-
-  if (Test-Path -Path $ZipFilePath) {
-    Write-Host "Successfully fetched $ZipFilePath. Extracting..."
-    Expand-Archive -Path $ZipFilePath -DestinationPath "./mage" -Force
-    chmod +x ./mage/mage
-  } else {
-    Write-Error "Failed to fetch $S3Key from S3."
-  }
+  Expand-Archive -Path mage.zip -DestinationPath ./mage/ -Force
+  chmod +x ./mage/mage
 
   $deployYamlsFound | ForEach-Object -Parallel {
     ins "./mage/mage octopus:octoPush $($_.FullName)" -ErrorOnFailure
   }
 }
 else {
-  throw "::error::No Deploy files (.yaml|.yml) files found in $env:DEPLOY_YAML_DIR"
+  throw "::error::No Deploy files (.yaml|.yml) files found in .variant folder"
 }
