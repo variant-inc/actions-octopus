e064b865e6654fd3eb0d493b833061f170ed29e1
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2025-10-17 09:55:06 -0400

    Updates S3 key and package pathing
    Updates the S3 key to include a directory prefix, ensuring proper organization and retrieval of mage-runner and dx-schemas packages.
    Adds a random filename to the unzip path to avoid potential naming conflicts when fetching packages from S3.
    
    Relates to CLOUD-2777

--- package.sh
+++ package.sh
@@ -4,7 +4,8 @@ set -e          # Exit on error
 set -u          # Treat unset variables as an error
 set -o pipefail # Exit on errors in pipelines
 
-full_unzip_path="$UNZIP_PATH/$S3_KEY"
+random_filename=$(openssl rand -hex 16)
+full_unzip_path="$UNZIP_PATH/${random_filename}.zip"
 
 echo "Fetching $S3_KEY from S3..."
 aws s3 cp "s3://$DX_PACKAGES_S3_BUCKET/$S3_KEY" "$full_unzip_path" --force
